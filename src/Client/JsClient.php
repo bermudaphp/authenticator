399874dc5f93f2928e9f57d46d3f01bcceebdf90
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-12-11 22:33:07 +0200

    Create JsClient.php

---
+++ src/Client/JsClient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bermuda\Authenticator\Client;
+
+use Bermuda\Authenticator\ClientInterface;
+
+final class JsClient implements ClientInterface
+{
+    public function __construct(public readonly string $id) {
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSecret(): ?string
+    {
+        return null;
+    }
+}
