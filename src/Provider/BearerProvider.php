3992fbf4f4b9c1bb7c438a7f4413cc419a409c12
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-12-11 22:38:20 +0200

    Create BearerProvider.php

---
+++ src/Provider/BearerProvider.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Bermuda\Authenticator\Provider;
+
+use Bermuda\Authenticator\Client\Client;
+use Bermuda\Authenticator\AuthenticationException;
+use Bermuda\Authenticator\AuthenticationProvider;
+use Bermuda\Authenticator\Token\BearerTokenGeneratorInterface;;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class BearerProvider implements AuthenticationProvider
+{
+    use ProviderTrait;
+    private const headerName = 'Authorization';
+    public function __construct(private readonly TokenMap $map, private BearerTokenGeneratorInterface $generator)
+    {
+    }
+
+    /**
+     * @param ContainerInterface $container
+     * @return static
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public static function fromContainer(ContainerInterface $container): self
+    {
+        $config = $container->get('config')['auth'];
+        return new self(new TokenMap($config['token-map']), $container->get(BearerTokenGeneratorInterface::class));
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return ServerRequestInterface
+     * @throws AuthenticationException
+     */
+    public function authentication(ServerRequestInterface $request): ServerRequestInterface
+    {
+        if (($token = $this->getTokenFromRequest($request)) !== null) {
+            if (!$this->map->has($token)) {
+                throw AuthenticationException::create('The token is missing from the map', 401, $request);
+            }
+
+            return $request->withAttribute(self::client_attribute,
+                new Client($this->map->getClientID($token), $token)
+            );
+        }
+
+        return $request;
+    }
+
+    /**
+     * @param ServerRequestInterface $serverRequest
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function unauthorized(ServerRequestInterface $serverRequest, ResponseInterface $response): ResponseInterface
+    {
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $serverRequest
+     * @param ResponseInterface $response
+     * @param \DateTimeInterface|int|null $expires
+     * @return ResponseInterface
+     */
+    public function write(ServerRequestInterface $serverRequest, ResponseInterface $response, \DateTimeInterface|int $expires = null): ResponseInterface
+    {
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $serverRequest
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function clear(ServerRequestInterface $serverRequest, ResponseInterface $response): ResponseInterface
+    {
+        return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $serverRequest
+     * @return bool
+     */
+    public function isAuthenticated(ServerRequestInterface $serverRequest): bool
+    {
+        return $serverRequest->getAttribute(self::client_attribute) instanceof Client;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return string|null
+     * @throws AuthenticationException
+     */
+    private function getTokenFromRequest(ServerRequestInterface $request):? string
+    {
+       if ($request->hasHeader(self::headerName)) {
+           $token = $this->generator->parseHeader($request->getHeaderLine(self::headerName));
+           if ($token === null) {
+               throw AuthenticationException::create('Invalid bearer token', 401, $request);
+           }
+
+           return $token;
+        }
+
+        return null;
+    }
+}
