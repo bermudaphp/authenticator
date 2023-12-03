1c80de12c7e3ac3ea5e3f1d2d6cbacc43cfee85f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-12-03 20:17:14 +0200

    Create Fields.php

---
+++ src/Handler/Fields.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bermuda\Authenticator\Handler;
+
+abstract class Fields
+{
+    public const identityField = 'identityField';
+    public const credentialsField = 'credentialsField';
+    public const languageField = 'languageField';
+}
